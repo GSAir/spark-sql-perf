a8719c8dfcf2c4b6c328531e719c440999059d1d
Gregory Essertel <gregory.essertel@gmail.com>
2016-02-12 13:32:21 -0500

    Extract to csv file

--- extractResult.py
+++ extractResult.py
@@ -10,11 +10,24 @@ if len(sys.argv) != 2:
 with open(sys.argv[1], 'r') as f:
     data = map(lambda x: json.loads(x)['results'], f.read().split('\n')[:-1])
 
+query = [None] * 22
+
+legend = ['optimization', 'execution', 'parsing', 'planning', 'analysis', 'total']
 
 for it, line in enumerate(data):
-    print "iteration {0}".format(it + 1)
     for num, queryresult in enumerate(line):
-        print "\tTPCH #{0}".format(num + 1)
+        if not query[num]:
+            query[num] = []
+        if len(query[num]) == it:
+            query[num].append([it])
         for key, val in queryresult.iteritems():
             if "Time" in key:
-                print "\t\t- {0}: {1}".format(key, val)
+                query[num][-1].append(val)
+
+
+with open('result.csv', 'wb') as f:
+    for num, q in enumerate(query):
+        f.write(str(num + 1) + ',' + ','.join(legend) + '\n')
+        tmp = [it[::] + [sum(it)] for it in q]
+        tmp.append([e / len(tmp) for e in reduce(lambda acc, l: map(lambda x, y: x + y, acc, l), tmp)])
+        f.write('\n'.join([','.join(map(str, l)) for l in tmp]) + '\n')
