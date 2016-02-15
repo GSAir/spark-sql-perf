372b0aba588632606f1e6bd6429b0bc17412d710
Gregory Essertel <gregory.essertel@gmail.com>
2016-02-15 09:42:44 -0500

    Extract to csv in order

--- extractResult.py
+++ extractResult.py
@@ -10,24 +10,36 @@ if len(sys.argv) != 2:
 with open(sys.argv[1], 'r') as f:
     data = map(lambda x: json.loads(x)['results'], f.read().split('\n')[:-1])
 
-query = [None] * 22
+query = {}
 
 legend = ['optimization', 'execution', 'parsing', 'planning', 'analysis', 'total']
 
 for it, line in enumerate(data):
-    for num, queryresult in enumerate(line):
-        if not query[num]:
+    print "iteration {0}".format(it + 1)
+    for queryresult in line:
+        num = queryresult['name']
+        print "\t{0}".format(num)
+        if num not in query:
             query[num] = []
         if len(query[num]) == it:
-            query[num].append([it])
+            query[num].append([it + 1])
         for key, val in queryresult.iteritems():
             if "Time" in key:
                 query[num][-1].append(val)
+                print "\t\t{0} {1}".format(key, val)
 
 
 with open('result.csv', 'wb') as f:
-    for num, q in enumerate(query):
-        f.write(str(num + 1) + ',' + ','.join(legend) + '\n')
+    i = 0
+    j = 0
+    while j < len(query):
+        i += 1
+        name = "tpch{0}".format(i)
+        if name not in query:
+            continue
+        q = query[name]
+        f.write(name + ',' + ','.join(legend) + '\n')
         tmp = [it[::] + [sum(it)] for it in q]
-        tmp.append([e / len(tmp) for e in reduce(lambda acc, l: map(lambda x, y: x + y, acc, l), tmp)])
-        f.write('\n'.join([','.join(map(str, l)) for l in tmp]) + '\n')
+        # tmp.append([e / len(tmp) for e in reduce(lambda acc, l: map(lambda x, y: x + y, acc, l), tmp)])
+        f.write('\n'.join([','.join(map(str, l)) for l in tmp]) + '\n\n')
+        j += 1
